78440b56862c4d64ad043192a3b16a3419d65464
Uhan7 <luckyfellowfella@gmail.com>
2025-06-16 23:43:30 +0800

    room made in transition

--- Assets/Scripts/Main Game/Room.cs
+++ Assets/Scripts/Main Game/Room.cs
@@ -6,17 +6,30 @@ public class Room : MonoBehaviour
     [SerializeField] private GameObject roomCamera;
     //[SerializeField] private float cameraSize;
 
+    [SerializeField] private GameObject exteriorBG;
+
+    private Animator exteriorBGAnim;
+    [SerializeField] private string roomFadeOutClip;
+    [SerializeField] private string roomFadeInClip;
+
+    private void Awake()
+    {
+        exteriorBGAnim = exteriorBG.GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag != "Player") return;
+        if (col.gameObject.tag != "Protag") return;
 
         roomCamera.SetActive(true);
+        exteriorBGAnim.Play(roomFadeOutClip);
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
-        if (col.gameObject.tag != "Player") return;
+        if (col.gameObject.tag != "Protag") return;
 
         roomCamera.SetActive(false);
+        exteriorBGAnim.Play(roomFadeInClip);
     }
 }
