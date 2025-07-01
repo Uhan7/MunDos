a4d77eb1deca2f50002a185bcea89c4fbe78cdef
Uhan7 <luckyfellowfella@gmail.com>
2025-07-01 17:39:12 +0800

    set up "Building" prefab

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -6,28 +6,11 @@ public class Room : MonoBehaviour
     [SerializeField] private GameObject roomCamera;
     //[SerializeField] private float cameraSize;
 
-    [SerializeField] private GameObject exteriorBG;
-
-    private Animator exteriorBGAnim;
-    [SerializeField] private string roomFadeOutClip;
-    [SerializeField] private string roomFadeInClip;
-
-    private void Awake()
-    {
-        if (exteriorBG != null) exteriorBGAnim = exteriorBG.GetComponent<Animator>();
-    }
-
-    private void Start()
-    {
-        if (exteriorBG != null) exteriorBG.SetActive(true); // Might leave it inactive on build
-    }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag != "Protag") return;
 
         roomCamera.SetActive(true);
-        if (exteriorBG != null) exteriorBGAnim.Play(roomFadeOutClip);
     }
 
     private void OnTriggerExit2D(Collider2D col)
@@ -35,6 +18,5 @@ public class Room : MonoBehaviour
         if (col.gameObject.tag != "Protag") return;
 
         roomCamera.SetActive(false);
-        if (exteriorBG != null) exteriorBGAnim.Play(roomFadeInClip);
     }
 }
