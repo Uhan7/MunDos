bd6fdce9c877bf37335c87004b07e9d5451c5ba1
solar-ym <yzabelle_montuerto@dlsu.edu.ph>
2025-07-05 03:37:53 +0800

    Some set up i got lazie

--- Assets/Scripts/UI/Menus/PauseMenuManager.cs
+++ Assets/Scripts/UI/Menus/PauseMenuManager.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PauseMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject saveUI;
+    [SerializeField] private GameObject settingsUI;
+
     private Animator animator;
     private bool active = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,5 +22,14 @@ public class PauseMenuManager : MonoBehaviour
     public void togglePauseScreen()
     {
         active = !active;
+    }
+
+    public void toggleSettingsScreen()
+    {
+        settingsUI.SetActive(!settingsUI.activeInHierarchy);
+    }
+    public void toggleSaveScreen()
+    {
+        saveUI.SetActive(!saveUI.activeInHierarchy);
     }
 }
