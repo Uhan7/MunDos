e652e8aa777a6b564ac67163400eb16604369754
Uhan7 <luckyfellowfella@gmail.com>
2025-07-12 13:33:57 +0800

    enclosed room prefab

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using NaughtyAttributes;
 
 public class Room : MonoBehaviour
 {
     [SerializeField] private GameObject roomCamera;
-    //[SerializeField] private float cameraSize;
+    [SerializeField] private bool isEnclosed;
+    [ShowIf("isEnclosed")] [SerializeField] private GameObject exteriorWalls;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -12,6 +14,7 @@ public class Room : MonoBehaviour
 
         roomCamera.GetComponent<CinemachineCamera>().Target.TrackingTarget = col.gameObject.transform;
         roomCamera.SetActive(true);
+        if (exteriorWalls != null) exteriorWalls.SetActive(false);
     }
 
     private void OnTriggerExit2D(Collider2D col)
@@ -19,5 +22,6 @@ public class Room : MonoBehaviour
         if (col.gameObject.tag != "Protag") return;
 
         roomCamera.SetActive(false);
+        if (exteriorWalls != null) exteriorWalls.SetActive(true);
     }
 }
