07886dce208da15833a0446d35b474bfc4fe6109
Uhan7 <luckyfellowfella@gmail.com>
2025-07-23 14:51:15 +0800

    Modified item.cs
    - has new variable called "Has Dialogue" to determine if it should destroy after or just daectiavte sprite and collider, this way, the children can remain alive

--- Assets/Scripts/Objects/Items/Item.cs
+++ Assets/Scripts/Objects/Items/Item.cs
@@ -4,6 +4,7 @@ public class Item : MonoBehaviour
 {
     [Header("Item Data Variables")]
     [SerializeField] public ItemData data;
+    [SerializeField] private bool hasDialogue;
 
     private void Start()
     {
@@ -26,8 +27,7 @@ public class Item : MonoBehaviour
         GetComponent<InteractableObject>().Interact();
         GetComponent<BoxCollider2D>().enabled = false;
 
-        // Put anims here that will make it fade out or whatever
-
-        Destroy(gameObject, 0.5f);
+        if (hasDialogue) GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+        else Destroy(gameObject);
     }
 }
