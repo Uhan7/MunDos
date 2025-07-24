9177323541a25d1e6159a719f41d6d1ca5c98ab3
Uhan7 <luckyfellowfella@gmail.com>
2025-07-24 13:56:34 +0800

    assembled build scenes, created title screen

---
+++ Assets/Scripts/Scenes/SceneTransitioner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    [Header("Extra Variables")]
+    [SerializeField] private GameObject transitionObject;
+    [SerializeField] private float transitionDelay;
+
+    [HideInInspector] private string sceneName;
+
+    public void SceneTransitionWrapper(string _sceneName)
+    {
+        sceneName = _sceneName;
+
+        transitionObject.SetActive(true);
+        Invoke("GoToScene", transitionDelay);
+    }
+
+    private void GoToScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
