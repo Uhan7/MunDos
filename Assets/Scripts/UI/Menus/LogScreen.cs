6d347bad3221edbaedb046e91a1bd6487fd38a42
solar-ym <yzabelle_montuerto@dlsu.edu.ph>
2025-09-19 01:49:36 +0800

    dia log stuff

--- Assets/Scripts/UI/Menus/LogScreen.cs
+++ Assets/Scripts/UI/Menus/LogScreen.cs
@@ -1,10 +1,12 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogScreen : MonoBehaviour
 {
     [SerializeField] private GameObject logScreen;
     [SerializeField] private TextMeshProUGUI fullLog;
+    [SerializeField] private Scrollbar scrollbar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +16,14 @@ public class LogScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            scrollbar.value += 0.0001f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            scrollbar.value -= 0.0001f;
+        }
     }
     public void toggleLogScreen()
     {
