098ab65fddf0bd55f72d19416d11ead17265798e
Uhan7 <luckyfellowfella@gmail.com>
2025-09-25 17:06:43 +0800

    basic text prmopt

---
+++ Assets/Scripts/UI/Dialogue/TextPrompt.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TextPrompt : MonoBehaviour
+{
+    [Header("Constants")]
+    [HideInInspector] private const string PROTAG_TAG = "Protag";
+
+    [Header("Prompt Values")]
+    [SerializeField] private GameObject promptBackground;
+    [SerializeField] private GameObject textPrompt;
+    [SerializeField] private float timeUntilDeactivate;
+
+    private void Start()
+    {
+        promptBackground.GetComponent<Animator>().Play("image_invisible");
+        textPrompt.GetComponent<Animator>().Play("text_invisible");
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.gameObject.CompareTag(PROTAG_TAG)) return;
+
+        promptBackground.GetComponent<Animator>().Play("image_fade_in_half");
+        textPrompt.GetComponent<Animator>().Play("text_fade_in_full");
+    }
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (!col.gameObject.CompareTag(PROTAG_TAG)) return;
+
+        promptBackground.GetComponent<Animator>().Play("image_fade_out_half");
+        textPrompt.GetComponent<Animator>().Play("text_fade_out_full");
+    }
+}
