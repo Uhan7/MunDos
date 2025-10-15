3e20c84fa8692c487b976707df15be03eeb2c5e2
Q <127469056+whatatat@users.noreply.github.com>
2025-10-15 18:40:07 +0800

    LockableObject fix

--- Assets/Scripts/Interactables/LockableObject.cs
+++ Assets/Scripts/Interactables/LockableObject.cs
@@ -9,9 +9,23 @@ public class LockableObject : MonoBehaviour
     [SerializeField] public int requiredChecks = 1;
     [HideInInspector] public int currentChecks = 0;
 
+    [Header("Flags")]
+    [HideInInspector] private bool isAlreadyLocked = false;
+    [HideInInspector] private bool hasStoredLockValue = false;
+    [HideInInspector] private bool storedLockValue = false;
+
     private void OnEnable()
     {
-        Lock(isLockedAtStart);
+        if (!isAlreadyLocked)
+        {
+            Lock(isLockedAtStart);
+            isAlreadyLocked = true;
+        }
+        if(hasStoredLockValue && currentChecks >= requiredChecks)
+        {
+            Lock(storedLockValue);
+        }
+        
     }
 
     // Helper Functions --------------------------------------------------------
@@ -19,5 +33,7 @@ public class LockableObject : MonoBehaviour
     public void Lock(bool val)
     {
         GetComponent<BoxCollider2D>().enabled = !val;
+        hasStoredLockValue = true;
+        storedLockValue = val;
     }
 }
