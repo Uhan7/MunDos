388d45d04e2c68cab4bfb4c3bc8867d5c6106691
solar-ym <yzabelle_montuerto@dlsu.edu.ph>
2025-10-23 01:02:14 +0800

    settings pls work girl omg

---
+++ Assets/Scripts/Main Managers/SettingsInfo.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class SettingsInfo
+{
+    public static float musicVol = 0.75f;
+    public static float SFXVol = 0.75f;
+    public static float dialogueVol = 1f;
+}
--- Assets/Scripts/Main Managers/SettingsManager.cs
+++ Assets/Scripts/Main Managers/SettingsManager.cs
@@ -1,18 +1,27 @@
+using NaughtyAttributes;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class SettingsManager : MonoBehaviour
 {
+    [SerializeField] private bool isTitleScreen;
+
+    [Header("Title Screen Settings")]
+    [ShowIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider musicVol;
+    [ShowIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider SFXVol;
+    [ShowIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider dialogueVol;
+
     [Header("Past Settings")]
-    [SerializeField] UnityEngine.UI.Slider pastMusicVol;
-    [SerializeField] UnityEngine.UI.Slider pastSFXVol;
-    [SerializeField] UnityEngine.UI.Slider pastDialogueVol;
+    [HideIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider pastMusicVol;
+    [HideIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider pastSFXVol;
+    [HideIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider pastDialogueVol;
 
     [Header("Present Settings")]
-    [SerializeField] UnityEngine.UI.Slider presentMusicVol;
-    [SerializeField] UnityEngine.UI.Slider presentSFXVol;
-    [SerializeField] UnityEngine.UI.Slider presentDialogueVol;
+    [HideIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider presentMusicVol;
+    [HideIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider presentSFXVol;
+    [HideIf("isTitleScreen")] [SerializeField] UnityEngine.UI.Slider presentDialogueVol;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,22 +31,71 @@ public class SettingsManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void setMusicVol(float vol)
     {
-        pastMusicVol.value = vol;
-        presentMusicVol.value = vol;
+        SettingsInfo.musicVol = vol;
+        if (!isTitleScreen)
+        {
+            pastMusicVol.value = vol;
+            presentMusicVol.value = vol;
+        } else
+        {
+            musicVol.value = vol;
+        }
     }
     public void setSFXVol(float vol)
     {
-        pastSFXVol.value = vol;
-        presentSFXVol.value = vol;
+        SettingsInfo.SFXVol = vol;
+        if (!isTitleScreen)
+        {
+            pastSFXVol.value = vol;
+            presentSFXVol.value = vol;
+        } else
+        {
+            SFXVol.value = vol;
+        }
     }
     public void setDialogueVol(float vol)
     {
-        pastDialogueVol.value = vol;
-        presentDialogueVol.value = vol;
+        SettingsInfo.dialogueVol = vol;
+        if (!isTitleScreen)
+        {
+            pastDialogueVol.value = vol;
+            presentDialogueVol.value = vol;
+        } else
+        {
+            dialogueVol.value = vol;
+        }
+    }
+
+    public void Sync()
+    {
+        if (!isTitleScreen) StartCoroutine(SyncSettings());
+        else StartCoroutine(SyncTitleSettings());
+    }
+
+    IEnumerator SyncSettings()
+    {
+        yield return new WaitForEndOfFrame();
+
+        pastMusicVol.value = SettingsInfo.musicVol;
+        presentMusicVol.value = SettingsInfo.musicVol;
+
+        pastSFXVol.value = SettingsInfo.SFXVol;
+        presentSFXVol.value = SettingsInfo.SFXVol;
+
+        pastDialogueVol.value = SettingsInfo.dialogueVol;
+        presentDialogueVol.value = SettingsInfo.dialogueVol;
+    }
+
+    IEnumerator SyncTitleSettings()
+    {
+        yield return new WaitForEndOfFrame();
+
+        musicVol.value = SettingsInfo.musicVol;
+        SFXVol.value = SettingsInfo.SFXVol;
+        dialogueVol.value = SettingsInfo.dialogueVol;
     }
 }
