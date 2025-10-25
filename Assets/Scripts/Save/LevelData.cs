bd4e2f67d6a7023f588a081f150bbad2e538e3bf
Q <127469056+whatatat@users.noreply.github.com>
2025-10-26 03:56:54 +0800

    present dz fix

--- Assets/Scripts/Save/LevelData.cs
+++ Assets/Scripts/Save/LevelData.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 
-public class LevelData : MonoBehaviour
+[System.Serializable]
+public class LevelData
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public InventorySlot[] SantiInventory;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+    public InventorySlot[] LiezelInventory;
+
+
+
 }
