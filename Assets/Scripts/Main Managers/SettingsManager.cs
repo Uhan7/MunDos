a8a19b5739ef0ca12a08b5e7e4e8b807845735bf
solar-ym <yzabelle_montuerto@dlsu.edu.ph>
2025-11-01 17:14:31 +0800

    Week 8 ISSUE-12 Re-Fix
    Settings in main menu immediately sync instead of only syncing after opening settings

--- Assets/Scripts/Main Managers/SettingsManager.cs
+++ Assets/Scripts/Main Managers/SettingsManager.cs
@@ -25,7 +25,7 @@ public class SettingsManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Sync();
     }
 
     // Update is called once per frame
