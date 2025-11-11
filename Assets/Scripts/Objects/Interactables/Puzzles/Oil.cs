d0348524d99240b379810794f4ca4ec861d5814e
Uhan7 <luckyfellowfella@gmail.com>
2025-11-11 11:22:48 +0800

    oildone

--- Assets/Scripts/Objects/Interactables/Puzzles/Oil.cs
+++ Assets/Scripts/Objects/Interactables/Puzzles/Oil.cs
@@ -3,18 +3,38 @@ using NaughtyAttributes;
 
 public class Oil : MonoBehaviour
 {
-    [Header("Oil Values")]
+    [Header("Oil Types")]
     [SerializeField] private bool isFromPast;
+
+    [Header("Oil Values")]
+    [HideIf("isFromPast")] [SerializeField] private float desiredPH;
+    [HideIf("isFromPast")] [SerializeField] private float desiredSalinity;
     [HideIf("isFromPast")] [SerializeField] private float originalPH;
     [HideIf("isFromPast")] [SerializeField] private float originalSalinity;
-    [HideIf("isFromPast")] [SerializeField] private float pH;
-    [HideIf("isFromPast")] [SerializeField] private float salinity;
+    [ReadOnly] [HideIf("isFromPast")] [SerializeField] private float pH;
+    [ReadOnly] [HideIf("isFromPast")] [SerializeField] private float salinity;
     [ShowIf("isFromPast")] [SerializeField] private Oil presentTimelineOil;
 
     [Header("Item Interact")]
-    [SerializeField] private GameObject itemDZ;
-    [SerializeField] [TextArea(3, 10)] private string originalItemDZSentence = "It changed to a pH of <oil_ph> and Salinity of <oil_salinity>.";
-    [HideInInspector] private string itemDZSentence;
+    [HideIf("isFromPast")] [SerializeField] [TextArea(3, 10)] private string originalOrchidSapDZSentence = "Original Orchid Sap Sentence.";
+    [HideIf("isFromPast")] [SerializeField] [TextArea(3, 10)] private string originalTubeDZSentence = "Original Oil Tube Sentence.";
+    [ShowIf("isFromPast")] [SerializeField] [TextArea(3, 10)] private string originalHibiscusSapDZSentence = "Original Hibiscus Sap Sentence.";
+    [ShowIf("isFromPast")] [SerializeField] [TextArea(3, 10)] private string originalHibiscusPetalDZSentence = "Original Hibiscus Petal Sentence.";
+    [SerializeField] private GameObject DZ;
+    [HideInInspector] private string DZSentence;
+
+    [Header("References")]
+    [HideIf("isFromPast")] [SerializeField] private BoxCollider2D[] otherOils;
+    [HideIf("isFromPast")] [SerializeField] private GameObject correctDZ;
+
+    [Header("Flags")]
+    [HideInInspector] private bool finished = false;
+
+    private void Start()
+    {
+        pH = originalPH;
+        salinity = originalSalinity;
+    }
 
     public void ItemInteract(string itemName)
     {
@@ -28,6 +48,20 @@ public class Oil : MonoBehaviour
                 }
                 pH += 0.3f;
                 salinity -= 0.01f;
+
+                DZSentence = originalOrchidSapDZSentence;
+                break;
+
+            case "Oil Tube":
+                if (isFromPast)
+                {
+                    Debug.LogError("Error in Oil.cs: Oil Tube should NOT be isFromPast");
+                    return;
+                }
+
+                DZSentence = originalTubeDZSentence.Replace("<oil_ph>", pH.ToString()).Replace("<oil_salinity>", salinity.ToString());
+
+                if (pH == desiredPH && salinity == desiredSalinity) PuzzleFinish();
                 break;
 
             case "Hibiscus Sap":
@@ -38,6 +72,8 @@ public class Oil : MonoBehaviour
                 }
                 presentTimelineOil.pH -= 0.1f;
                 presentTimelineOil.salinity += 0.03f;
+
+                DZSentence = originalHibiscusSapDZSentence;
                 break;
 
             case "Hibiscus Petal":
@@ -46,11 +82,14 @@ public class Oil : MonoBehaviour
                     Debug.LogError("Error in Oil.cs: Hibiscus Petal should be isFromPast");
                     return;
                 }
-                presentTimelineOil.pH = originalPH;
-                presentTimelineOil.salinity = originalSalinity;
+                presentTimelineOil.pH = presentTimelineOil.originalPH;
+                presentTimelineOil.salinity = presentTimelineOil.originalSalinity;
+
+                DZSentence = originalHibiscusPetalDZSentence;
                 break;
 
-            default: break;
+            default:
+                break;
         }
 
         pH = Mathf.Round(pH * 100) / 100;
@@ -58,8 +97,18 @@ public class Oil : MonoBehaviour
         if (presentTimelineOil != null) presentTimelineOil.pH = Mathf.Round(presentTimelineOil.pH * 100) / 100;
         if (presentTimelineOil != null) presentTimelineOil.salinity = Mathf.Round(presentTimelineOil.salinity * 100) / 100;
 
-        itemDZSentence = originalItemDZSentence.Replace("<oil_ph>", pH.ToString()).Replace("<oil_salinity>", salinity.ToString());
-        itemDZ.GetComponent<DialogueTrigger>().dialogue.sentences[0] = itemDZSentence;
-        itemDZ.SetActive(true);
+        if (!finished)
+        {
+            DZ.GetComponent<DialogueTrigger>().dialogue.sentences[0] = DZSentence;
+            DZ.SetActive(true);
+        }
+    }
+
+    void PuzzleFinish()
+    {
+        foreach (BoxCollider2D col in otherOils) col.enabled = false;
+        correctDZ.SetActive(true);
+
+        finished = true;
     }
 }
