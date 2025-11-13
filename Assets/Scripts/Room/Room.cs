5bc9a11bf8b152746678709e7b3c5f89da0f38ba
Uhan7 <luckyfellowfella@gmail.com>
2025-11-13 11:16:11 +0800

    area bgm transition works, will connect to full map

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Unity.Cinemachine;
 using NaughtyAttributes;
@@ -5,6 +6,7 @@ using NaughtyAttributes;
 public class Room : MonoBehaviour
 {
     [SerializeField] private GameObject roomCamera;
+    [SerializeField] private AudioSource areaBGMSource;
     [SerializeField] private GameObject[] parallaxObjects;
     [SerializeField] private bool showFirst;
 
@@ -15,6 +17,8 @@ public class Room : MonoBehaviour
         roomCamera.GetComponent<CinemachineCamera>().Target.TrackingTarget = col.gameObject.transform;
 
         roomCamera.SetActive(true);
+        if (areaBGMSource != null) StartCoroutine(FadeInBGM(areaBGMSource, 2f));
+
         if (parallaxObjects.Length > 0) foreach (GameObject parallaxObject in parallaxObjects)
         {
                 if (!showFirst) parallaxObject.SetActive(false);
@@ -27,5 +31,44 @@ public class Room : MonoBehaviour
         if (col.gameObject.tag != "Protag") return;
 
         roomCamera.SetActive(false);
+        if (areaBGMSource != null) StartCoroutine(FadeOutBGM(areaBGMSource, 2f));
+    }
+
+    // Helper Functions and Coroutines
+
+    IEnumerator FadeOutBGM(AudioSource bgmSource, float fadeDuration)
+    {
+        float time = 0f;
+        float startVolOut = bgmSource.volume;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / fadeDuration;
+
+            bgmSource.volume = Mathf.Lerp(startVolOut, 0f, t);
+
+            yield return null;
+        }
+
+        bgmSource.volume = 0f;
+    }
+
+    IEnumerator FadeInBGM(AudioSource bgmSource, float fadeDuration)
+    {
+        float time = 0f;
+        float startVolIn = bgmSource.volume;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / fadeDuration;
+
+            bgmSource.volume = Mathf.Lerp(startVolIn, SettingsInfo.musicVol, t);
+
+            yield return null;
+        }
+
+        bgmSource.volume = SettingsInfo.musicVol;
     }
 }
