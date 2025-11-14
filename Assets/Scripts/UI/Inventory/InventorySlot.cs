345a7e0c0b5b178d77d2fbecab520a9652d6444a
solar-ym <yzabelle_montuerto@dlsu.edu.ph>
2025-11-15 01:48:40 +0800

    ok der
    basically just undid everything nicholas did...

--- Assets/Scripts/UI/Inventory/InventorySlot.cs
+++ Assets/Scripts/UI/Inventory/InventorySlot.cs
@@ -5,16 +5,6 @@ using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
-    [Header("Zoom Components")]
-    [HideInInspector] float SCREEN_WIDTH;
-    [HideInInspector] float SCREEN_HEIGHT;
-    [HideInInspector] float REFERENCE_WIDTH = 1080;
-    [HideInInspector] float REFERENCE_HEIGHT = 1920;
-    [HideInInspector] float REFERENCE_RATIO;
-    [HideInInspector] float ASPECT_RATIO;
-    [HideInInspector] float ZOOM_PERCENTAGE = 0.70f;
-
-
     [Header("Components")]
     [HideInInspector] private Image imageComponent;
     [SerializeField] public GameObject zoomObject;
@@ -43,28 +33,9 @@ public class InventorySlot : MonoBehaviour
 
     private void Update()
     {
-        if (Screen.width != SCREEN_WIDTH) 
-        {
-            SCREEN_WIDTH = Screen.width;
-            test = true;
-        }
-        if (Screen.height != SCREEN_HEIGHT) 
-        {
-            SCREEN_HEIGHT = Screen.height;
-            test = true;
-        }
-        if (test) 
-        { 
-            Debug.Log($"screen width {SCREEN_WIDTH} height {SCREEN_HEIGHT}, % width {SCREEN_WIDTH * ZOOM_PERCENTAGE}, height {SCREEN_HEIGHT * ZOOM_PERCENTAGE}"); 
-            test = false; 
-            ASPECT_RATIO = REFERENCE_WIDTH / REFERENCE_HEIGHT;
-        }
     }
     private void Start()
     {
-        REFERENCE_RATIO = REFERENCE_WIDTH / REFERENCE_HEIGHT;
-        SCREEN_WIDTH = Screen.width;
-        SCREEN_HEIGHT = Screen.height;
         if (zoomObject == null) Debug.LogError($"Error {this.name}'s zoomObject is null");
     }
 
@@ -88,23 +59,6 @@ public class InventorySlot : MonoBehaviour
     {
         Focus(value);
         zoomObject.SetActive(value);
-        Sprite sprite = zoomObject.GetComponent<UnityEngine.UI.Image>().sprite;
-        //RectTransform rectTransform = sprite.GetComponent<RectTransform>();
-        ////image.preserveAspect = true;
-        //Vector2 imageSize = rectTransform.sizeDelta;
-        //float factor;
-        //if (imageSize.y >= imageSize.x)
-        //{
-        //    factor = (SCREEN_WIDTH * ZOOM_PERCENTAGE) / imageSize.y;
-        //}
-        //else
-        //{
-        //    factor = (SCREEN_HEIGHT * ZOOM_PERCENTAGE) / imageSize.x;
-        //}
-
-        //imageSize.x = imageSize.x * factor;
-        //imageSize.y = imageSize.y * factor;
-        //rectTransform.sizeDelta = new Vector2(imageSize.x, imageSize.y);
     }
     public void OnZoom()
     {
