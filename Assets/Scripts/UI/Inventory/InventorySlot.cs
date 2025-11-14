3af6899f071762d1757cf6003e4865439f1f36cb
Q <127469056+whatatat@users.noreply.github.com>
2025-11-15 01:12:56 +0800

    zoom pls

--- Assets/Scripts/UI/Inventory/InventorySlot.cs
+++ Assets/Scripts/UI/Inventory/InventorySlot.cs
@@ -1,8 +1,20 @@
+using NUnit.Framework.Constraints;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
+    [Header("Zoom Components")]
+    [HideInInspector] float SCREEN_WIDTH;
+    [HideInInspector] float SCREEN_HEIGHT;
+    [HideInInspector] float REFERENCE_WIDTH = 1080;
+    [HideInInspector] float REFERENCE_HEIGHT = 1920;
+    [HideInInspector] float REFERENCE_RATIO;
+    [HideInInspector] float ASPECT_RATIO;
+    [HideInInspector] float ZOOM_PERCENTAGE = 0.70f;
+
+
     [Header("Components")]
     [HideInInspector] private Image imageComponent;
     [SerializeField] public GameObject zoomObject;
@@ -18,6 +30,7 @@ public class InventorySlot : MonoBehaviour
     [SerializeField] private GameObject[] toActivateOnZoom;
     [SerializeField] private GameObject[] toDeactivateOnZoom;
 
+    bool test = false;
     private void Awake()
     {
         if (gameObject.activeInHierarchy) InitializeComponents();
@@ -28,8 +41,30 @@ public class InventorySlot : MonoBehaviour
         if (imageComponent == null) InitializeComponents();
     }
 
+    private void Update()
+    {
+        if (Screen.width != SCREEN_WIDTH) 
+        {
+            SCREEN_WIDTH = Screen.width;
+            test = true;
+        }
+        if (Screen.height != SCREEN_HEIGHT) 
+        {
+            SCREEN_HEIGHT = Screen.height;
+            test = true;
+        }
+        if (test) 
+        { 
+            Debug.Log($"screen width {SCREEN_WIDTH} height {SCREEN_HEIGHT}, % width {SCREEN_WIDTH * ZOOM_PERCENTAGE}, height {SCREEN_HEIGHT * ZOOM_PERCENTAGE}"); 
+            test = false; 
+            ASPECT_RATIO = REFERENCE_WIDTH / REFERENCE_HEIGHT;
+        }
+    }
     private void Start()
     {
+        REFERENCE_RATIO = REFERENCE_WIDTH / REFERENCE_HEIGHT;
+        SCREEN_WIDTH = Screen.width;
+        SCREEN_HEIGHT = Screen.height;
         if (zoomObject == null) Debug.LogError($"Error {this.name}'s zoomObject is null");
     }
 
@@ -53,6 +88,23 @@ public class InventorySlot : MonoBehaviour
     {
         Focus(value);
         zoomObject.SetActive(value);
+        Sprite sprite = zoomObject.GetComponent<UnityEngine.UI.Image>().sprite;
+        //RectTransform rectTransform = sprite.GetComponent<RectTransform>();
+        ////image.preserveAspect = true;
+        //Vector2 imageSize = rectTransform.sizeDelta;
+        //float factor;
+        //if (imageSize.y >= imageSize.x)
+        //{
+        //    factor = (SCREEN_WIDTH * ZOOM_PERCENTAGE) / imageSize.y;
+        //}
+        //else
+        //{
+        //    factor = (SCREEN_HEIGHT * ZOOM_PERCENTAGE) / imageSize.x;
+        //}
+
+        //imageSize.x = imageSize.x * factor;
+        //imageSize.y = imageSize.y * factor;
+        //rectTransform.sizeDelta = new Vector2(imageSize.x, imageSize.y);
     }
     public void OnZoom()
     {
