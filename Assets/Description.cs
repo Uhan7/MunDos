af515122710110654458d1a674dd69793c3818aa
Q <127469056+whatatat@users.noreply.github.com>
2025-11-17 05:28:09 +0800

    Past 5
    L2 Area Beneath Distressed Woman (3) not activating. Will check later
    
    L2 Painting Envi (3) doesn't have an activation yet.
    
    Double check if activating gameobject then  activating it's child component after valid

--- Assets/Description.cs
+++ Assets/Description.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 public class Description : MonoBehaviour
 {
     [TextArea(6, 12)]
-    [SerializeField] private string descriptionText = "Activated By:\n\nLocked/Unlocked By:\n\nConditional Check By:";
+    [SerializeField] private string descriptionText = "Activated/Deactivated By:\n\nLocked/Unlocked By:\n\nConditional Check By:";
 }
