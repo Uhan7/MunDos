869dfacd9673ef5ff106d19b61d0755398857b9f
Uhan7 <luckyfellowfella@gmail.com>
2025-11-20 17:32:54 +0800

    liezel's stuff also added

--- Assets/Scripts/UI/Camera/CameraEffectsManager.cs
+++ Assets/Scripts/UI/Camera/CameraEffectsManager.cs
@@ -15,7 +15,7 @@ public class CameraEffectsManager : MonoBehaviour
     [SerializeField] private AudioClip flashSFX;
     [SerializeField] private AudioClip dimSFX;
 
-    [Header("Voices SFX")]
+    [Header("Voices SFX: Santi")]
     [SerializeField] private AudioClip santiConcernedSFX;
     [SerializeField] private AudioClip santiFrustratedSFX;
     [SerializeField] private AudioClip santiHappySFX;
@@ -23,10 +23,24 @@ public class CameraEffectsManager : MonoBehaviour
     [SerializeField] private AudioClip santiQuestioningSFX;
     [SerializeField] private AudioClip santiRelievedSFX;
     [SerializeField] private AudioClip santiSatisfiedSFX;
-    [SerializeField] private AudioClip santiGaspSFX;
+    [SerializeField] private AudioClip santiShockGaspSFX;
     [SerializeField] private AudioClip santiSighSFX;
     [SerializeField] private AudioClip santiThinkingSFX;
     [SerializeField] private AudioClip santiWonderingSFX;
+
+    [Header("Voices SFX: Liezel")]
+    [SerializeField] private AudioClip liezelConcernedSFX;
+    [SerializeField] private AudioClip liezelDiscoverySFX;
+    [SerializeField] private AudioClip liezelFrustratedSFX;
+    [SerializeField] private AudioClip liezelHappySFX;
+    [SerializeField] private AudioClip liezelOhSFX;
+    [SerializeField] private AudioClip liezelQuestioningSFX;
+    [SerializeField] private AudioClip liezelRelievedSFX;
+    [SerializeField] private AudioClip liezelSatisfiedAhaSFX;
+    [SerializeField] private AudioClip liezelSatisfiedHaSFX;
+    [SerializeField] private AudioClip liezelShockGaspSFX;
+    [SerializeField] private AudioClip liezelThinkingSFX;
+    [SerializeField] private AudioClip liezelWonderingSFX;
 
     // Helper Functions --------------------------------------------------------
 
@@ -113,7 +127,33 @@ public class CameraEffectsManager : MonoBehaviour
     {
         switch (audioClipName)
         {
+            // Santi
             case "<SFX_santi_concerned>": sfxSource.PlayOneShot(santiConcernedSFX); break;
+            case "<SFX_santi_frustrated>": sfxSource.PlayOneShot(santiFrustratedSFX); break;
+            case "<SFX_santi_happy>": sfxSource.PlayOneShot(santiHappySFX); break;
+            case "<SFX_santi_oh>": sfxSource.PlayOneShot(santiOhSFX); break;
+            case "<SFX_santi_questioning>": sfxSource.PlayOneShot(santiQuestioningSFX); break;
+            case "<SFX_santi_relieved>": sfxSource.PlayOneShot(santiRelievedSFX); break;
+            case "<SFX_santi_satisfied>": sfxSource.PlayOneShot(santiSatisfiedSFX); break;
+            case "<SFX_santi_shock_gasp>": sfxSource.PlayOneShot(santiShockGaspSFX); break;
+            case "<SFX_santi_sigh>": sfxSource.PlayOneShot(santiSighSFX); break;
+            case "<SFX_santi_thinking>": sfxSource.PlayOneShot(santiThinkingSFX); break;
+            case "<SFX_santi_wondering>": sfxSource.PlayOneShot(santiWonderingSFX); break;
+
+            // Liezel
+            case "<SFX_liezel_concerned>": sfxSource.PlayOneShot(liezelConcernedSFX); break;
+            case "<SFX_liezel_discovery>": sfxSource.PlayOneShot(liezelDiscoverySFX); break;
+            case "<SFX_liezel_frustrated>": sfxSource.PlayOneShot(liezelFrustratedSFX); break;
+            case "<SFX_liezel_happy>": sfxSource.PlayOneShot(liezelHappySFX); break;
+            case "<SFX_liezel_oh>": sfxSource.PlayOneShot(liezelOhSFX); break;
+            case "<SFX_liezel_questioning>": sfxSource.PlayOneShot(liezelQuestioningSFX); break;
+            case "<SFX_liezel_relieved>": sfxSource.PlayOneShot(liezelRelievedSFX); break;
+            case "<SFX_liezel_satisfied_(aha!)>": sfxSource.PlayOneShot(liezelSatisfiedAhaSFX); break;
+            case "<SFX_liezel_satisfied_(ha!)>": sfxSource.PlayOneShot(liezelSatisfiedHaSFX); break;
+            case "<SFX_liezel_shock_gasp>": sfxSource.PlayOneShot(liezelShockGaspSFX); break;
+            case "<SFX_liezel_thinking>": sfxSource.PlayOneShot(liezelThinkingSFX); break;
+            case "<SFX_liezel_wondering>": sfxSource.PlayOneShot(liezelWonderingSFX); break;
+
             default: break;
         }
     }
