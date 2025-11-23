ef14436f10b0e382e47145e9071152afcafc81e1
Uhan7 <luckyfellowfella@gmail.com>
2025-11-24 03:06:12 +0800

    go go yza

--- Assets/Scripts/Debugs/TemporaryDialogue.cs
+++ Assets/Scripts/Debugs/TemporaryDialogue.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 public class TemporaryDialogue : MonoBehaviour
 {
     [SerializeField] private bool hasProperDialogue;
+
+    // HELO YZA tbh I'm not sure if some of these should even contain dialogue
+    // and it's a whole nother step to check the story docs so,,, imma leave this to u
     [SerializeField] private bool containsSFX;
 }
