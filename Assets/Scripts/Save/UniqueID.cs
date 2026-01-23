c2c8c7e195bd046f17f9383d73b36c53a5cb08cd
Uhan7 <luckyfellowfella@gmail.com>
2026-01-23 22:01:18 +0800

    BOOM SHAKALAKA IT CAN BE ATTACHED TO PREFABS NOW
    not gon push yet tho, I'll do it all in one go with all the objects

--- Assets/Scripts/Save/UniqueID.cs
+++ Assets/Scripts/Save/UniqueID.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using System;
 
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
 [DisallowMultipleComponent]
 public class UniqueID : MonoBehaviour
 {
     [SerializeField] private string id;
-
     public string ID => id;
 
 #if UNITY_EDITOR
-    void OnValidate()
+    private void OnValidate()
     {
+        if (PrefabUtility.IsPartOfPrefabAsset(this)) return;
+        if (PrefabStageUtility.GetPrefabStage(gameObject) != null) return;
+
         if (string.IsNullOrEmpty(id)) id = Guid.NewGuid().ToString();
     }
 #endif
-
 }
