bb7a83a3799f75a0718d52dabbebf42a3c0aa7a0
solar-ym <168715276+solar-ym@users.noreply.github.com>
2026-03-05 19:27:19 +0800

    exit game

---
+++ Assets/Scripts/UI/Menus/ExitPrompt.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ExitPrompt : MonoBehaviour
+{
+    public void LeaveApplication()
+    {
+        // Other processes to stop here
+
+        Application.Quit();
+    }
+}
