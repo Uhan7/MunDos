b7380f085f00707719a57a53bdbb86094fe64282
Uhan7 <luckyfellowfella@gmail.com>
2026-07-05 15:12:20 +0800

    HOPEFULLY RANDOM DIALOGUE WONT OPEN NAYMMORE

--- Assets/Scripts/Save/Split Components/DialogueTriggerSaveData.cs
+++ Assets/Scripts/Save/Split Components/DialogueTriggerSaveData.cs
@@ -2,7 +2,7 @@
 
 public class DialogueTriggerSaveData : ComponentSaveData
 {
-    public bool dialogueIsTriggered;
+    //public bool dialogueIsTriggered;
     public bool dialogueAlreadyActivatedObjects;
     public bool dialogueAlreadyDeactivatedObjects;
     public bool dialogueAlreadyCheckedConditional;
